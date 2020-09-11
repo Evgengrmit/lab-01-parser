d93904b36a5adc40e44631cc64bf104a96970102
Evgengrmit <evgengrmit@gmail.com>
2020-09-11 20:47:31 +0300

    Add Parser

---
+++ include/parser.hpp
@@ -0,0 +1,42 @@
+// Copyright 2020 Evgenij Grigorev evgengrmit@icloud.com
+
+#ifndef INCLUDE_PARSER_HPP_
+#define INCLUDE_PARSER_HPP_
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "student.hpp"
+
+struct Lengths_of_fields {
+  size_t length_1_field;
+  size_t length_2_field;
+  size_t length_3_field;
+  size_t length_4_field;
+};
+class Parser {
+ public:
+  Parser();
+  explicit Parser(const std::vector<Student> &s);
+  explicit Parser(const std::string &path);
+  ~Parser();
+  [[nodiscard]] const std::vector<Student> &getStudents() const;
+  [[nodiscard]] const Lengths_of_fields &getL() const;
+  void printRow(const Student &student) const;
+  [[nodiscard]] std::string getSeparator() const;
+  void parser(const std::string &path);
+  void sortByName();
+  void sortByAverageScore();
+  bool emptyJSONobject() const;
+  void setJSONstring(const std::string &JSON);
+
+ private:
+  std::vector<Student> students;
+  Lengths_of_fields l{15, 8, 6, 15};
+  void setLengths();
+};
+
+#endif  // INCLUDE_PARSER_HPP_
